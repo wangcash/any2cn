d623350fd40526bbeafa42c681e65ef68283aa88
wangcash <wangcash@gmail.com>
2013-06-16 17:40:32 +0800

    修改BingModule功能。
    只进行英文搜索，之前是默认值进行中文搜索。

--- app/controllers/bing_module.rb
+++ app/controllers/bing_module.rb
@@ -13,7 +13,7 @@ module BingModule
   # 只调网页搜索接口，每月配额5000。
   def search_by_bing(query)
     begin
-      form = URI.encode_www_form([[:Query, "'#{query}'"]]) #转义query
+      form = URI.encode_www_form([[:Query, "'#{query}'"], [:Market, "'es-US'"]]) #转义query
       uri  = "#{BING_SEARCH_API}#{form}"
       resp = open(uri, {http_basic_authentication: [BING_APP_ID, BING_APP_ID]}) #访问接口、权限验证
       json = JSON.parse(resp.read)
